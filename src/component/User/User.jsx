3e4f73e7be87a476260b07159b9c22cc76872483
Reanne Matias <starosamatias@gmail.com>
2024-08-31 10:54:55 +0800

    Initial commit

---
+++ src/component/User/User.jsx
@@ -0,0 +1,9 @@
+function User() {
+    return (
+        <>
+            <h1>Reanne Ashley S.R. Matias</h1>
+            <h2>BSIT3B</h2>
+            <p>Short Description</p></>
+    )
+}
+export default User;
